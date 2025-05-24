f1d089c4dc05797b74cec81ad8048a089a3237f8
Alvi305 <alvirahman305@gmail.com>
2025-05-24 18:18:29 +0800

    feat: added page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import { sampleProducts } from './data'
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 
@@ -21,23 +22,10 @@ function App() {
       </header>
       <main>
         <Container className="mt-3">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image"
-                />
-                <h2>{product.name}</h2>
-                <p>${product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet />
         </Container>
       </main>
       <footer>
-        {' '}
         <div className="text-center">All rights reserved</div>
       </footer>
     </div>
