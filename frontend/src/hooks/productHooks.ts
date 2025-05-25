edfb7e1a986d29515a85cd9e203aac97e6b889ed
Alvi305 <alvirahman305@gmail.com>
2025-05-26 01:39:16 +0800

    feat: added product page

--- frontend/src/hooks/productHooks.ts
+++ frontend/src/hooks/productHooks.ts
@@ -3,7 +3,14 @@ import apiClient from '../apiClient'
 import { Product } from '../types/Product'
 
 export const useGetProductsQuery = () =>
-    useQuery({
-        queryKey: ['products'],
-        queryFn: async () => (await apiClient.get<Product[]>(`api/products`)).data,
-        })
+  useQuery({
+    queryKey: ['products'],
+    queryFn: async () => (await apiClient.get<Product[]>(`api/products`)).data,
+  })
+
+export const useGetProductDetailsBySlugQuery = (slug: string) =>
+  useQuery({
+    queryKey: ['products', slug],
+    queryFn: async () =>
+      (await apiClient.get<Product>(`api/products/${slug}`)).data,
+  })
